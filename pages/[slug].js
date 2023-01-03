3e54d9296c2a175f900d54f667953ce3a9efb52a
Hayato Kamiyama <kami8ma8810@gmail.com>
2023-01-03 11:39:02 +0900

    chore:prettier関連追加

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,10 @@
-import Layout from '@/layouts/layout'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
-import BLOG from '@/blog.config'
-import { createHash } from 'crypto'
+import Layout from '@/layouts/layout';
+import { getAllPosts, getPostBlocks } from '@/lib/notion';
+import BLOG from '@/blog.config';
+import { createHash } from 'crypto';
 
 const BlogPost = ({ post, blockMap, emailHash }) => {
-  if (!post) return null
+  if (!post) return null;
   return (
     <Layout
       blockMap={blockMap}
@@ -12,31 +12,31 @@ const BlogPost = ({ post, blockMap, emailHash }) => {
       emailHash={emailHash}
       fullWidth={post.fullWidth}
     />
-  )
-}
+  );
+};
 
-export async function getStaticPaths () {
-  const posts = await getAllPosts({ includePages: true })
+export async function getStaticPaths() {
+  const posts = await getAllPosts({ includePages: true });
   return {
-    paths: posts.map(row => `${BLOG.path}/${row.slug}`),
-    fallback: true
-  }
+    paths: posts.map((row) => `${BLOG.path}/${row.slug}`),
+    fallback: true,
+  };
 }
 
-export async function getStaticProps ({ params: { slug } }) {
-  const posts = await getAllPosts({ includePages: true })
-  const post = posts.find(t => t.slug === slug)
-  const blockMap = await getPostBlocks(post.id)
+export async function getStaticProps({ params: { slug } }) {
+  const posts = await getAllPosts({ includePages: true });
+  const post = posts.find((t) => t.slug === slug);
+  const blockMap = await getPostBlocks(post.id);
   const emailHash = createHash('md5')
     .update(BLOG.email)
     .digest('hex')
     .trim()
-    .toLowerCase()
+    .toLowerCase();
 
   return {
     props: { post, blockMap, emailHash },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
 
-export default BlogPost
+export default BlogPost;
